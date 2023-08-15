1f2d3c382ba4e60487cfab165c93f5858e305192
SJ <wxxsij001@myuct.ac.za>
2023-08-15 12:59:43 +0200

    refactor: adjustments after merge

--- src/app/trust-line-creator-form/trust-line-creator-form.component.ts
+++ src/app/trust-line-creator-form/trust-line-creator-form.component.ts
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { SessionXrplService } from 'src/app/state/session-xrpl.service';
 import { SetupQuery } from 'src/app/state/setup';
 import { environment } from 'src/environments/environment';
 import { IssuedCurrencyAmount } from 'xrpl/dist/npm/models/common';
-=======
-import { Component } from '@angular/core';
 import { SessionService } from 'src/app/state/session.service';
-import { SwalHelper } from 'src/app/utils/notification/swal-helper';
->>>>>>> cd374acadb14e27d0ba3038b711bcc1a82f4901d
+import { SwalHelper } from 'src/app/utils/notification/swal-helper ';
 
 @Component({
   selector: 'app-trust-line-creator-form',
