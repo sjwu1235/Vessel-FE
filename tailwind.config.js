7d8301d767338b8cb37917b27695e6979b9642b3
SJ <wxxsij001@myuct.ac.za>
2023-08-16 23:27:09 +0200

    style: sub tailwind theme for general

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'orange': '#ff642e',
-      'white': '#ffffff',
-      'black': '#000000',
-      'dark-gray': '#1e1f21',
-      'light-gray': '#b2b2b2',
-      'dark-blue': '#003450',
-      'light-blue': '#013f61',
-      'turquoise': '#42f595',
+      'primary-a': '#ff642e', //orange
+      'primary-t': '#ffffff', //white
+      'primary-c': '#000000', //black
+      'secondary-dg': '#1e1f21', //dark gray
+      'secondary-lg': '#b2b2b2', //light gray
+      'primary-b': '#003450', //dark blue
+      'secondary-b': '#013f61', //light blue
+      'accent-b': '#42f595', //turqoise
     },
     extend: {
       fontFamily: {
