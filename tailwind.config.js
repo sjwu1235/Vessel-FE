0e00f7b48ec180841d3a13a7cea496e98fce71c7
SJ <wxxsij001@myuct.ac.za>
2023-08-17 09:17:47 +0200

    style: switch for generic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,53 @@ module.exports = {
         'xs': '475px',
         ...defaultTheme.screens,
       },
+    // colors: {
+    //   'primary-a': '#ff642e', //orange
+    //   'primary-d': '#ff642e', //orange
+    //   'primary-t': '#ffffff', //white
+    //   'primary-c': '#000000', //black
+    //   'primary-x': '#003450', //skyfall
+    //   'secondary-dg': '#1e1f21', //dark gray
+    //   'secondary-lg': '#b2b2b2', //light gray
+    //   'primary-b': '#003450', //dark blue
+    //   'secondary-b': '#013f61', //light blue
+    //   'secondary-d': '#ff642e', //orange
+    //   'accent-b': '#42f595', //turqoise
+    //   'error-t': '#ff642e', //orange
+    // },
+    // colors: {
+    //   'primary-a': '#56bdc1', //turqoise
+    //   'primary-d': '#fcd77b', //yellow
+    //   'primary-t': '#ffffff', //white
+    //   'primary-c': '#000000', //black
+    //   'primary-x': '#1a1632', //skyfall
+    //   'secondary-dg': '#1e1f21', //dark gray
+    //   'secondary-lg': '#b2b2b2', //light gray
+    //   'primary-b': '#1a1632', //skyfall
+    //   'secondary-b': '#2f2759', //light blue
+    //   'secondary-d': '#ffffff', //white
+    //   'accent-b': '#42f595', //turqoise
+    //   'error-t': '#ff642e', //error
+    // },
     colors: {
-      'primary-a': '#ff642e', //orange
-      'primary-t': '#ffffff', //white
-      'primary-c': '#000000', //black
+      'primary-a': '#56bdc1', //turqoise
+      'primary-d': '#ff642e', //yellow
+      'primary-t': '#1a1632', //white
+      'primary-c': '#1a1632', //black
+      'primary-x': '#ffffff', //white
       'secondary-dg': '#1e1f21', //dark gray
       'secondary-lg': '#b2b2b2', //light gray
-      'primary-b': '#003450', //dark blue
-      'secondary-b': '#013f61', //light blue
+      'primary-b': '#ffffff', //skyfall
+      'secondary-b': '#2f2759', //light blue
+      'secondary-d': '#ff642e', //white
       'accent-b': '#42f595', //turqoise
-      'error-t': '#ff642e', //orange
+      'error-t': '#ff642e', //error
     },
     extend: {
       fontFamily: {
         primary: "nasalization",
         audiowide: "Audiowide, cursive",
+        monserrat: "montserrat"
       },
       space: {
         '5px': '5px',
@@ -36,7 +68,15 @@ module.exports = {
         '108': '27rem',
         '112': '28rem',
         '128': '32rem'
-      }
+      },
+      // boxShadow: {
+      //   'inner-xl': '0 0 0 50px inset #1a1632 !important',
+      //   'inner-sm': '0 0 0 2px #1a1632a2 !important'
+      // },
+      boxShadow: {
+        'inner-xl': '0 0 0 50px inset #ffffff !important',
+        'inner-sm': '0 0 0 2px #ffffffa2 !important'
+      },
     },
   },
   plugins: [],
