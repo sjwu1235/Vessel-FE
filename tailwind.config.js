63f2b5f5a724bc3ad5d81bcaa31005b9503b3d4a
SJ <wxxsij001@myuct.ac.za>
2023-08-18 18:35:01 +0200

    refactor: highlights

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,8 @@ module.exports = {
         'xs': '475px',
         ...defaultTheme.screens,
       },
-    // colors: {
-    //   'primary-a': '#ff642e', //orange
-    //   'primary-d': '#ff642e', //orange
-    //   'primary-t': '#ffffff', //white
-    //   'primary-c': '#000000', //black
-    //   'primary-x': '#003450', //skyfall
-    //   'secondary-dg': '#1e1f21', //dark gray
-    //   'secondary-lg': '#b2b2b2', //light gray
-    //   'primary-b': '#003450', //dark blue
-    //   'secondary-b': '#013f61', //light blue
-    //   'secondary-d': '#ff642e', //orange
-    //   'accent-b': '#42f595', //turqoise
-    //   'error-t': '#ff642e', //orange
-    // },
     colors: {
-      'primary-a': '#56bdc1', //turqoise
+      'primary-a': '#5bcacf', //turqoise
       'primary-d': '#fcd77b', //yellow
       'primary-t': '#ffffff', //white
       'primary-c': '#000000', //black
@@ -39,20 +25,6 @@ module.exports = {
       'accent-b': '#fcd77b', //turqoise
       'error-t': '#ff642e', //error
     },
-    // colors: {
-    //   'primary-a': '#56bdc1', //turqoise
-    //   'primary-d': '#ff642e', //yellow
-    //   'primary-t': '#1a1632', //white
-    //   'primary-c': '#1a1632', //black
-    //   'primary-x': '#ffffff', //white
-    //   'secondary-dg': '#1e1f21', //dark gray
-    //   'secondary-lg': '#b2b2b2', //light gray
-    //   'primary-b': '#ffffff', //skyfall
-    //   'secondary-b': '#2f2759', //light blue
-    //   'secondary-d': '#ff642e', //white
-    //   'accent-b': '#42f595', //turqoise
-    //   'error-t': '#ff642e', //error
-    // },
     extend: {
       fontFamily: {
         primary: "nasalization",
